7bfae174c7c1aa229743b2e4806143d1144ee035
Pongracz Noemi <pongracznoemiantonia@gmail.com>
2022-03-16 19:29:57 +0200

    storing data in an array

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-let addButton = document.getElementById("add-button");
-let taskList = document.getElementById("task-list");
+let todoList = [{title:'Reading', checked:false, id:1, notes:'', priority:'low'}, {title:'Hiking', checked:true, id:2, notes:'', priority:'low'}];
 
-addButton.addEventListener('click', createNewTask);
 
-function createNewTask(){
+window.addEventListener('load', (event) => {
+    let addButton = document.getElementById("add-button");
+    addButton.addEventListener('click', preventEmptyInput);
+    createTodoList();
+});
 
-    verifyIfTasklistExist();
+function preventEmptyInput(){
 
-    // ----- get value from input
     var inputValue = document.getElementById("input-value").value;
 
     if(inputValue === ''){
@@ -15,57 +16,102 @@ function createNewTask(){
         alert("You must write something!");
 
     } else {
-        
-        // ----- create elements
-        var newTaskElement = document.createElement("div");
-        newTaskElement.className = "task";
-        taskList.appendChild(newTaskElement);
+        addTodo(inputValue);
+    }
+}
+
+function createTodoList(){
+
+    if(Boolean(todoList)){
+        // create a div for all of the todos
+        todoListDiv = document.createElement("div");
+        todoListDiv.setAttribute("id", "task-list");
+        document.body.appendChild(todoListDiv);
+
+        // create DOM elements for all the todos from our array
+        todoList.forEach(todo => createDOMelement(todo));
+    }
+}
+
+function addToTodoList(todo){
+    todoList.push(todo);
+    console.log(todoList);
+}
+
+function addTodo(title){
+
+    const todo = {
+        title: title,
+        checked: false,
+        id: Date.now(),
+        notes: '',
+        priority: 'low'
+    }
+
+    // update the array of todos
+    addToTodoList(todo);
+
+    // create DOM element
+    createDOMelement(todo);
+
+    // clean the input
+    document.getElementById("input-value").value = '';
+}
+
+function deleteTodo(id){
+    var indexOfTodo =  todoList.findIndex(element => element.id === id );
+    todoList.splice( indexOfTodo, 1);
+    console.log(todoList)
+}
+
+function createDOMelement(todo){
+
+        var newTodoElement = document.createElement("div");
+        newTodoElement.className = "task";
+        todoListDiv.appendChild(newTodoElement);
 
         var newCheckBox = document.createElement('input');
         newCheckBox.type = 'checkbox'; 
-        newTaskElement.appendChild(newCheckBox);
+        newTodoElement.appendChild(newCheckBox);
+        newCheckBox.checked = todo.checked;
 
         var newEditableLabel = document.createElement('input');
         newEditableLabel.type = 'text';
-        newEditableLabel.value = inputValue;
-        newTaskElement.appendChild(newEditableLabel);
+        newEditableLabel.value = todo.title;
+        newTodoElement.appendChild(newEditableLabel);
 
         var newDeleteButton = document.createElement('img');
         newDeleteButton.src="bin.png";
         newDeleteButton.classList.add('delete-btn');
-        newTaskElement.appendChild(newDeleteButton);
-        
-    }
-    
-    // ----- clean the input
-    document.getElementById("input-value").value = '';
+        newTodoElement.appendChild(newDeleteButton);
 
+        handleCheckboxStates(newCheckBox, newEditableLabel);
 
-    // ----- handle checkbox states
-    newCheckBox.addEventListener('change', (event) => {
-        console.log(event);
-        if(newCheckBox.checked){
-            newEditableLabel.readOnly= 'readOnly';
-            newEditableLabel.classList.add("strikethrough","hideborder");
-        }
-        else{
-            newEditableLabel.removeAttribute('readOnly');
-            newEditableLabel.classList.remove("strikethrough","hideborder");
-        }
-    })
+        newCheckBox.addEventListener('change', ()=>{ changeCheckedStatus(todo.id); handleCheckboxStates(newCheckBox, newEditableLabel);});
 
-    newDeleteButton.addEventListener('click', (event)=>{
-        console.log(event);
-        event.target.parentElement.remove();
-    });
+        newDeleteButton.addEventListener('click', (event)=>{
+            deleteTodo(todo.id);
+            event.target.parentElement.remove();
+        });
 }
 
-
-function verifyIfTasklistExist(){
-    if(!Boolean(taskList)){
-        console.log("tasklist doesn't exist yet");
-        taskList = document.createElement("div");
-        taskList.setAttribute("id", "task-list");
-        document.body.appendChild(taskList);
+function handleCheckboxStates(checkboxElement, inputElement){
+    if(checkboxElement.checked){
+        inputElement.readOnly= 'readOnly';
+        inputElement.classList.add("strikethrough","hideborder");
     }
+    else{
+        inputElement.removeAttribute('readOnly');
+        inputElement.classList.remove("strikethrough","hideborder");
+    }
+}
+
+function changeCheckedStatus(id){
+    var indexOfTodo =  todoList.findIndex(element => element.id === id );
+
+    if(todoList[indexOfTodo].checked)
+        todoList[indexOfTodo].checked = false;
+    else
+        todoList[indexOfTodo].checked = true;
+    console.log(todoList);
 }
